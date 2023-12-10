9fdc77a2d25e2c03f827a33de82533c907c2d0ee
tranphu <tranphudhak@gmail.com>
2023-12-10 15:32:31 +0700

    fix: edit font family of app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import AppRoutes from "./routes";
 import './common/customStyle.css'
 function App() {
   return (
-    <div className="App" style={{fontFamily: 'Quicksand'}}>
+    <div className="App">
       <AppRoutes />
     </div>
   );
