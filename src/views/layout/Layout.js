304808e5528ea78e030052e41b6f48883536dc10
tranphu <tranphudhak@gmail.com>
2023-12-23 21:09:08 +0700

    reload page when logging out

--- src/views/layout/Layout.js
+++ src/views/layout/Layout.js
@@ -24,6 +24,7 @@ export default function Layout({
   const handleLogout = () => {
     setIsAuth(false);
     localStorage.removeItem("user");
+    window.location.reload();
   };
 
   useEffect(() => {
